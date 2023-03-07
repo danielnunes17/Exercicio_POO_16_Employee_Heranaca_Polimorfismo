75e7725424c0ec8c0b46a7e9f293646eb69a7a35
Daniel Stephanio Nunes <dti584@localiza.com>
2023-03-07 17:56:50 -0300

    b

--- Program.cs
+++ Program.cs
@@ -13,29 +13,36 @@ public class Program
             Console.WriteLine($"Employee #{i} data: ");
             Console.Write("Outsourced (y/n)? ");
             char outsourced = char.Parse(Console.ReadLine());
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Hours: ");
-            int hours = int.Parse(Console.ReadLine());
-            Console.Write("Value per hour: ");
-            double valueHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            if (outsourced == 'y')
-            {
-                Console.Write("Additional charge: ");
-                double aditionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                employees.Add(new OutSourceEmployee(name, hours, valueHour, aditionalCharge));
-            }
-            else
-            {
-                employees.Add(new Employee(name, hours, valueHour));
-            }
 
-            Console.WriteLine();
-            Console.WriteLine("PAYMENTS: ");
-            foreach (Employee employee in employees)
-            {
-                Console.WriteLine(employee.Name + " - $" + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
-            }
+            if (outsourced == 'y')
+                employees.Add(CreateOutsourcedEmployee());
+            else
+                employees.Add(CreateEmployee());
         }
+        Console.WriteLine();
+        Console.WriteLine("PAYMENTS: ");
+        foreach (Employee employee in employees)
+        {
+            Console.WriteLine(employee.Name + " - $" + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+
+    private static OutSourceEmployee CreateOutsourcedEmployee()
+    {
+        Employee employed = CreateEmployee();
+        Console.Write("Additional charge: ");
+        double aditionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        return new OutSourceEmployee(employed.Name, employed.Hour, employed.VeluePerHours, aditionalCharge);
+    }
+
+    private static Employee CreateEmployee()
+    {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Hours: ");
+        int hours = int.Parse(Console.ReadLine());
+        Console.Write("Value per hour: ");
+        double valueHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        return new Employee(name, hours, valueHour);
     }
 }
